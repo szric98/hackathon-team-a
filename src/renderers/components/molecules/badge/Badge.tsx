0383d4a924d95b0e00229db486105b266b3a09bf
Mike <mpurgatori@gmail.com>
2025-02-26 10:10:02 +0000

    more packaged

--- src/renderers/components/molecules/badge/Badge.tsx
+++ src/renderers/components/molecules/badge/Badge.tsx
@@ -1,125 +1,145 @@
-import type { WithAnalyticsTag } from "@/types";
 import { cva } from "class-variance-authority";
 import type { FC } from "react";
 import { twMerge } from "tailwind-merge";
 import { Icon } from "../../atoms/icons/Icon";
 import type { AvailableIcons } from "../../atoms/icons/types";
 import { Typography } from "../../atoms/typographies/Typography";
+import type { WithAnalyticsTag } from "../../../types";
 
-export const COLOR_VALUES = ["default", "gray", "red", "yellow", "green"] as const;
+export const COLOR_VALUES = [
+	"default",
+	"gray",
+	"red",
+	"yellow",
+	"green",
+] as const;
 type COLOR = (typeof COLOR_VALUES)[number];
 
 export const SIZE_VALUES = ["sm", "lg"] as const;
 type SIZE = (typeof SIZE_VALUES)[number];
 
 export type BadgeProps = {
-  label: string;
-  color?: COLOR;
-  size?: SIZE;
-  icon?: AvailableIcons;
-  iconPosition?: "left" | "right";
-  className?: string;
-  onClick?: VoidFunction;
-  action?: WithAnalyticsTag<{ onClick: React.MouseEventHandler<SVGElement>; icon?: AvailableIcons }>;
+	label: string;
+	color?: COLOR;
+	size?: SIZE;
+	icon?: AvailableIcons;
+	iconPosition?: "left" | "right";
+	className?: string;
+	onClick?: VoidFunction;
+	action?: WithAnalyticsTag<{
+		onClick: React.MouseEventHandler<SVGElement>;
+		icon?: AvailableIcons;
+	}>;
 };
 
 export const Badge: FC<BadgeProps> = ({
-  color = "default",
-  size = "sm",
-  icon,
-  iconPosition,
-  className,
-  label,
-  action,
-  onClick,
+	color = "default",
+	size = "sm",
+	icon,
+	iconPosition,
+	className,
+	label,
+	action,
+	onClick,
 }) => {
-  const badgeClassName = badgeVariants({ color, size, clickable: !!onClick });
-  const primaryIconClassname = primaryIconVariants({ size, position: iconPosition });
-  const secondaryIconClassName = secondaryIconVariants({ color, size });
+	const badgeClassName = badgeVariants({ color, size, clickable: !!onClick });
+	const primaryIconClassname = primaryIconVariants({
+		size,
+		position: iconPosition,
+	});
+	const secondaryIconClassName = secondaryIconVariants({ color, size });
 
-  const BadgeTypography = typographyForSize(size);
+	const BadgeTypography = typographyForSize(size);
 
-  return (
-    <BadgeTypography className={twMerge(badgeClassName, className)} onClick={onClick}>
-      {icon && <Icon icon={icon} className={primaryIconClassname} />}
-      {label}
-      {action && (
-        <Icon
-          role="button"
-          icon={action.icon ?? "XOutlineIcon"}
-          className={secondaryIconClassName}
-          onClick={action.onClick}
-          dataAnalyticsId={action.dataAnalyticsId}
-        />
-      )}
-    </BadgeTypography>
-  );
+	return (
+		<BadgeTypography
+			className={twMerge(badgeClassName, className)}
+			onClick={onClick}
+		>
+			{icon && <Icon icon={icon} className={primaryIconClassname} />}
+			{label}
+			{action && (
+				<Icon
+					role="button"
+					icon={action.icon ?? "XOutlineIcon"}
+					className={secondaryIconClassName}
+					onClick={action.onClick}
+					dataAnalyticsId={action.dataAnalyticsId}
+				/>
+			)}
+		</BadgeTypography>
+	);
 };
 
 type VariantSettings = {
-  color: Record<COLOR, string>;
-  size: Record<SIZE, string>;
+	color: Record<COLOR, string>;
+	size: Record<SIZE, string>;
 };
 
 type BadgeVariant = VariantSettings & {
-  clickable: Record<"true" | "false", string>;
+	clickable: Record<"true" | "false", string>;
 };
-const badgeVariants = /*tw:*/ cva<BadgeVariant>("inline-flex items-center gap-1 rounded-md", {
-  variants: {
-    color: {
-      default:
-        'bg-brand-100 text-brand-800 data-[clickable="true"]:hover:text-brand-500 dark:bg-brand-900 dark:text-brand-300',
-      gray: 'bg-gray-100 text-gray-600 data-[clickable="true"]:hover:text-gray-500 dark:bg-gray-700 dark:text-gray-300',
-      red: 'bg-red-100 text-red-800 data-[clickable="true"]:hover:text-red-500 dark:bg-red-900 dark:text-red-300',
-      yellow:
-        'bg-yellow-100 text-yellow-800 data-[clickable="true"]:hover:text-yellow-400 dark:bg-yellow-900 dark:text-yellow-300',
-      green:
-        'bg-green-100 text-green-800 data-[clickable="true"]:hover:text-green-500 dark:bg-green-900 dark:text-green-300',
-    },
-    size: {
-      sm: "px-2.5 py-0.5",
-      lg: "px-3 py-0.5",
-    },
-    clickable: {
-      true: "cursor-pointer",
-      false: "",
-    },
-  },
-});
+const badgeVariants = /*tw:*/ cva<BadgeVariant>(
+	"inline-flex items-center gap-1 rounded-md",
+	{
+		variants: {
+			color: {
+				default:
+					'bg-brand-100 text-brand-800 data-[clickable="true"]:hover:text-brand-500 dark:bg-brand-900 dark:text-brand-300',
+				gray: 'bg-gray-100 text-gray-600 data-[clickable="true"]:hover:text-gray-500 dark:bg-gray-700 dark:text-gray-300',
+				red: 'bg-red-100 text-red-800 data-[clickable="true"]:hover:text-red-500 dark:bg-red-900 dark:text-red-300',
+				yellow:
+					'bg-yellow-100 text-yellow-800 data-[clickable="true"]:hover:text-yellow-400 dark:bg-yellow-900 dark:text-yellow-300',
+				green:
+					'bg-green-100 text-green-800 data-[clickable="true"]:hover:text-green-500 dark:bg-green-900 dark:text-green-300',
+			},
+			size: {
+				sm: "px-2.5 py-0.5",
+				lg: "px-3 py-0.5",
+			},
+			clickable: {
+				true: "cursor-pointer",
+				false: "",
+			},
+		},
+	},
+);
 
-type PrimaryIconVariant = Pick<VariantSettings, "size"> & { position: Record<"left" | "right", string> };
+type PrimaryIconVariant = Pick<VariantSettings, "size"> & {
+	position: Record<"left" | "right", string>;
+};
 const primaryIconVariants = /*tw:*/ cva<PrimaryIconVariant>("fill-current", {
-  variants: {
-    position: {
-      left: "order-first",
-      right: "order-last",
-    },
-    size: {
-      sm: "size-2.5",
-      lg: "size-3",
-    },
-  },
+	variants: {
+		position: {
+			left: "order-first",
+			right: "order-last",
+		},
+		size: {
+			sm: "size-2.5",
+			lg: "size-3",
+		},
+	},
 });
 
 const secondaryIconVariants = /*tw:*/ cva<VariantSettings>("cursor-pointer", {
-  variants: {
-    color: {
-      default: "fill-brand-500 dark:fill-brand-300",
-      gray: "fill-gray-500 dark:fill-gray-500",
-      red: "fill-red-500 dark:fill-red-300",
-      yellow: "fill-yellow-400 dark:fill-yellow-300",
-      green: "fill-green-500 dark:fill-green-300",
-    },
-    size: {
-      sm: "size-2",
-      lg: "size-2.5",
-    },
-  },
+	variants: {
+		color: {
+			default: "fill-brand-500 dark:fill-brand-300",
+			gray: "fill-gray-500 dark:fill-gray-500",
+			red: "fill-red-500 dark:fill-red-300",
+			yellow: "fill-yellow-400 dark:fill-yellow-300",
+			green: "fill-green-500 dark:fill-green-300",
+		},
+		size: {
+			sm: "size-2",
+			lg: "size-2.5",
+		},
+	},
 });
 
 const typographyForSize = (size: SIZE) => {
-  if (size === "sm") return Typography.SubCaptionMedium;
-  if (size === "lg") return Typography.CaptionMedium;
-  const _never = size;
-  throw new Error(`Unexpected size: ${_never}`);
+	if (size === "sm") return Typography.SubCaptionMedium;
+	if (size === "lg") return Typography.CaptionMedium;
+	const _never = size;
+	throw new Error(`Unexpected size: ${_never}`);
 };
--- src/renderers/components/molecules/breadcrumb/Breadcrumb.tsx
+++ src/renderers/components/molecules/breadcrumb/Breadcrumb.tsx
@@ -7,64 +7,88 @@ import { Dropdown, type DropdownMenuItem } from "../dropdown/Dropdown";
 import { DropdownIconTrigger } from "../dropdown/DropdownIconTrigger";
 
 export type BreadcrumbProps = {
-  items: { itemLabel: string; onClick?: VoidFunction; dropdownMenuItems?: DropdownMenuItem[] }[];
-  withBackground?: boolean;
-  icon?: AvailableIcons;
+	items: {
+		itemLabel: string;
+		onClick?: VoidFunction;
+		dropdownMenuItems?: DropdownMenuItem[];
+	}[];
+	withBackground?: boolean;
+	icon?: AvailableIcons;
 };
 
-export const Breadcrumb: FC<BreadcrumbProps> = ({ items, withBackground, icon }) => (
-  <nav
-    className={
-      withBackground
-        ? "flex w-fit rounded-lg border border-gray-200 bg-gray-50 px-4 py-3 dark:border-gray-700 dark:bg-gray-800"
-        : "flex"
-    }
-    aria-label="Breadcrumb"
-  >
-    <ol className="inline-flex items-center space-x-1 md:space-x-2 rtl:space-x-reverse">
-      {items.map(({ itemLabel, onClick, dropdownMenuItems }, index) => (
-        <li key={uuidv4()} onClick={onClick} className="group inline-flex cursor-pointer items-center">
-          {index > 0 && <Icon icon="BreadcrumbArrowIcon" />}
-          {index === 0 && icon && (
-            <Icon icon={icon} className="mr-1 size-3 fill-gray-500 group-hover:fill-brand-700 dark:fill-gray-400 " />
-          )}
-          <span className={textVariants({ isLink: !!onClick, isDropdown: !!dropdownMenuItems?.length })}>
-            {itemLabel}
-          </span>
-          {dropdownMenuItems && dropdownMenuItems.length > 1 && (
-            <Dropdown
-              Trigger={
-                <DropdownIconTrigger
-                  icon="ChevronSortIcon"
-                  dataAnalyticsId="breadcrumb_navigation-dropdown_trigger"
-                  size="xs"
-                />
-              }
-              ariaLabel="Breadcrumb navigation dropdown"
-              dataAnalyticsId="breadcrumb_navigation-dropdown"
-              menuItems={dropdownMenuItems}
-            />
-          )}
-        </li>
-      ))}
-    </ol>
-  </nav>
+export const Breadcrumb: FC<BreadcrumbProps> = ({
+	items,
+	withBackground,
+	icon,
+}) => (
+	<nav
+		className={
+			withBackground
+				? "flex w-fit rounded-lg border border-gray-200 bg-gray-50 px-4 py-3 dark:border-gray-700 dark:bg-gray-800"
+				: "flex"
+		}
+		aria-label="Breadcrumb"
+	>
+		<ol className="inline-flex items-center space-x-1 md:space-x-2 rtl:space-x-reverse">
+			{items.map(({ itemLabel, onClick, dropdownMenuItems }, index) => (
+				// biome-ignore lint/a11y/useKeyWithClickEvents: <explanation>
+				<li
+					key={uuidv4()}
+					onClick={onClick}
+					className="group inline-flex cursor-pointer items-center"
+				>
+					{index > 0 && <Icon icon="BreadcrumbArrowIcon" />}
+					{index === 0 && icon && (
+						<Icon
+							icon={icon}
+							className="mr-1 size-3 fill-gray-500 group-hover:fill-brand-700 dark:fill-gray-400 "
+						/>
+					)}
+					<span
+						className={textVariants({
+							isLink: !!onClick,
+							isDropdown: !!dropdownMenuItems?.length,
+						})}
+					>
+						{itemLabel}
+					</span>
+					{dropdownMenuItems && dropdownMenuItems.length > 1 && (
+						<Dropdown
+							Trigger={
+								<DropdownIconTrigger
+									icon="ChevronSortIcon"
+									dataAnalyticsId="breadcrumb_navigation-dropdown_trigger"
+									size="xs"
+								/>
+							}
+							ariaLabel="Breadcrumb navigation dropdown"
+							dataAnalyticsId="breadcrumb_navigation-dropdown"
+							menuItems={dropdownMenuItems}
+						/>
+					)}
+				</li>
+			))}
+		</ol>
+	</nav>
 );
 
 type VariantSettings = {
-  isLink: Record<"true" | "false", string>;
-  isDropdown: Record<"true" | "false", string>;
+	isLink: Record<"true" | "false", string>;
+	isDropdown: Record<"true" | "false", string>;
 };
 
-const textVariants = /*tw:*/ cva<VariantSettings>("ms-1 font-medium text-sm md:ms-2 dark:text-gray-400", {
-  variants: {
-    isLink: {
-      true: "text-gray-700 group-hover:text-brand-700 dark:hover:text-white",
-      false: "text-gray-500 hover:text-gray-500 hover:dark:text-gray-400",
-    },
-    isDropdown: {
-      true: "mr-1.5",
-      false: "",
-    },
-  },
-});
+const textVariants = /*tw:*/ cva<VariantSettings>(
+	"ms-1 font-medium text-sm md:ms-2 dark:text-gray-400",
+	{
+		variants: {
+			isLink: {
+				true: "text-gray-700 group-hover:text-brand-700 dark:hover:text-white",
+				false: "text-gray-500 hover:text-gray-500 hover:dark:text-gray-400",
+			},
+			isDropdown: {
+				true: "mr-1.5",
+				false: "",
+			},
+		},
+	},
+);
