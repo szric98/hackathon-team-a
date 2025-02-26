35719311163b6bb07377ce3ba1b355ce72a626d1
Mike <mpurgatori@gmail.com>
2025-02-26 15:11:00 +0000

    sprintPosts

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,34 @@
+import { useState } from "react";
 import { Typography } from "./renderers/components/atoms/typographies/Typography";
 import { Badge } from "./renderers/components/molecules/badge/Badge";
 import { Button } from "./renderers/components/molecules/button/Button";
 import { RetroConfig } from "./RetroConfig";
 import { SprintRiskLineChart } from "./SprintRiskLineChart";
+import { postSprintCount } from "./connections/SprintPost";
 
-function App() {
-  return (
-    <div className="flex flex-col gap-6 p-6 bg-gray-50">
-      <div className="flex justify-between items-center">
-        <div className="flex items-center gap-3">
-          <Typography.Heading2>Run a Retro</Typography.Heading2>
-          <Badge label="BETA" />
-        </div>
+export default function App() {
+	const [postSprintState, setPostSprintState] = useState<unknown>(null);
 
-        <Button color="alternative">Share</Button>
-      </div>
+	console.log(postSprintState);
 
-      <RetroConfig />
+	return (
+		<div className="flex flex-col gap-6 p-6 bg-gray-50">
+			<div className="flex justify-between items-center">
+				<div className="flex items-center gap-3">
+					<Typography.Heading2>Run a Retro</Typography.Heading2>
+					<Badge label="BETA" />
+				</div>
 
-      <SprintRiskLineChart />
-    </div>
-  );
+				<Button color="alternative">Share</Button>
+
+				<Button onClick={() => setPostSprintState(postSprintCount(1))}>
+					Click me
+				</Button>
+			</div>
+
+			<RetroConfig />
+
+			<SprintRiskLineChart />
+		</div>
+	);
 }
-
-export default App;
---
+++ src/connections/SprintPost.ts
@@ -0,0 +1,24 @@
+export const postSprintCount = async (sprintCount: number) => {
+	try {
+		const response = await fetch("http://localhost:55000/sprints", {
+			method: "POST",
+			mode: "no-cors",
+			headers: {
+				"Content-Type": "application/json",
+			},
+			body: JSON.stringify({
+				n_sprints: sprintCount,
+			}),
+		});
+
+		if (!response.ok) {
+			throw new Error(`HTTP error! status: ${response.status}`);
+		}
+
+		const data = await response.json();
+		return data;
+	} catch (error) {
+		console.error("Error:", error);
+		throw error;
+	}
+};
